9158a1afe8b7ecc1c795ba5110548bcee20528e7
mstapf-eng <60630614+mstapf-eng@users.noreply.github.com>
2021-03-17 10:28:52 -0400

    Create simply_server.py

---
+++ transciever_comm/microphone/code/simply_server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import logging
+import numpy as np
+from numpysocket import NumpySocket
+
+logger = logging.getLogger('simple server')
+logger.setLevel(logging.INFO)
+
+npSocket = NumpySocket()
+
+logger.info("starting server, waiting for client")
+npSocket.startServer(9999)
+
+frame = npSocket.recieve()
+logger.info("array recieved:")
+logger.info(frame)
+
+logger.info("closing connection")
+
+try:
+    npSocket.close()
+except OSError as err:
+    logging.error("server already disconnected")
