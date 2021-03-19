515e9756da89aa58b6b4a0b84266b63c6658c788
patcav123 <70293447+patcav123@users.noreply.github.com>
2021-03-19 14:28:19 -0400

    Update LETdec.py

--- sat_tracking/LETdec.py
+++ sat_tracking/LETdec.py
@@ -40,33 +40,6 @@ def decode(txtfile):
 
     lengthSt = len(station)
 
-    return station, catalogNumber, classification, desigYR, desigNU, desigP, epochYR, epochDY, firstDer, secDer, drag, ephType, elNum, checkSum1, inclination, rightAsc, ecc, Per, Anom, Mot, revNum, checkSum2, lengthSt
+    return station, catalogNumber, checkSum1,checkSum2, lengthSt
 
-
-'''
-    for i in range(0, lengthSt-1):
-        print("Station:", station[i])
-        print("Satellite number:", catalogNumber[i])
-        print("Classification (U = Unclassified):", classification[i])
-        print("International Designator (Last 2 digits of launch year):", desigYR[i])
-        print("International Designator (Launch number of the year):", desigNU[i])
-        print("International Designator (Piece of the launch):", desigP[i])
-        print("Epoch Year (Last two digits of year):", epochYR[i])
-        print("Epoch (Day of the year and fractional portion of the day):", epochDY[i])
-        print("First Time Derivative of the Mean Motion:", firstDer[i])
-        print("Second Time Derivative of Mean Motion (Leading decimal point assumed):", secDer[i])
-        print("BSTAR drag term (Leading decimal point assumed):", drag[i])
-        print("Ephemeris type:", ephType[i])
-        print("Element number:", elNum[i])
-        print("Checksum (Modulo 10) (Letters, blanks, periods, plus signs = 0; minus signs = 1):", checkSum1[i])
-        print("Inclination [Degrees]:", inclination[i])
-        print("Right Ascension of the Ascending Node [Degrees]:", rightAsc[i])
-        print("Eccentricity (Leading decimal point assumed):", ecc[i])
-        print("Argument of Perigee [Degrees]:", Per[i])
-        print("Mean Anomaly [Degrees]:", Anom[i])
-        print("Mean Motion [Revs per day]:", Mot[i])
-        print("Revolution number at epoch [Revs]:", revNum[i])
-        print("Checksum (Modulo 10):", checkSum2[i], '\n')
-
-    '''
 
